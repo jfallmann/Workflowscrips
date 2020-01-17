3c78209ec549a6973f5d6b7adc6f1714ae0d1760
jfallmann <fall@bioinf.uni-leipzig.de>
2020-01-17 16:56:01 +0100

    formatting

--- Analysis/DEU/prepare_dexseq_annotation2.py
+++ Analysis/DEU/prepare_dexseq_annotation2.py
@@ -5,15 +5,15 @@ import sys, collections, itertools, os.path, optparse, os, gzip # changed here t
 
 optParser = optparse.OptionParser(
 
-   usage = "python %prog [options] <in.gtf> <out.gff>",
+    usage = "python %prog [options] <in.gtf> <out.gff>",
 
-   description=
+    description=
     "Script to prepare annotation for DEXSeq." +
     "This script takes an annotation file in Ensembl GTF format" +
     "and outputs a 'flattened' annotation file suitable for use " +
     "with the count_in_exons.py script ",
 
-   epilog =
+    epilog =
     "Written by Simon Anders (sanders@fs.tum.de), European Molecular Biology " +
     "Laboratory (EMBL). (c) 2010. Released under the terms of the GNU General " +
     "Public License v3. Part of the 'DEXSeq' package. " +
@@ -76,19 +76,19 @@ for f in HTSeq.GFF_Reader( gtf_file ):
 
 if aggregateGenes == True:
     gene_sets = collections.defaultdict( lambda: set() )
-   for iv, s in exons.steps():
-       # For each step, make a set, 'full_set' of all the gene IDs occuring
-       # in the present step, and also add all those gene IDs, whch have been
-       # seen earlier to co-occur with each of the currently present gene IDs.
-      full_set = set()
-      for gene_id, transcript_id in s:
-          full_set.add( gene_id )
-          full_set |= gene_sets[ gene_id ]
+    for iv, s in exons.steps():
+        # For each step, make a set, 'full_set' of all the gene IDs occuring
+        # in the present step, and also add all those gene IDs, whch have been
+        # seen earlier to co-occur with each of the currently present gene IDs.
+        full_set = set()
+        for gene_id, transcript_id in s:
+            full_set.add( gene_id )
+            full_set |= gene_sets[ gene_id ]
           # Make sure that all genes that are now in full_set get associated
           # with full_set, i.e., get to know about their new partners
-      for gene_id in full_set:
-          assert gene_sets[ gene_id ] <= full_set
-          gene_sets[ gene_id ] = full_set
+        for gene_id in full_set:
+            assert gene_sets[ gene_id ] <= full_set
+            gene_sets[ gene_id ] = full_set
 
 
 # Step 3: Go through the steps again to get the exonic sections. Each step
@@ -101,57 +101,57 @@ if aggregateGenes == True:
 aggregates = collections.defaultdict( lambda: list() )
 for iv, s in exons.steps( ):
     # Skip empty steps
-   if len(s) == 0:
-      continue
-  gene_id = list(s)[0][0]
-  ## if aggregateGenes=FALSE, ignore the exons associated to more than one gene ID
-   if aggregateGenes == False:
-       check_set = set()
-      for geneID, transcript_id in s:
-          check_set.add( geneID )
-      if( len( check_set ) > 1 ):
-         continue
-      else:
-          aggregate_id = gene_id
-          # Take one of the gene IDs, find the others via gene sets, and
-          # form the aggregate ID from all of them
-   else:
-       assert set( gene_id for gene_id, transcript_id in s ) <= gene_sets[ gene_id ]
-       aggregate_id = '+'.join( gene_sets[ gene_id ] )
-       # Make the feature and store it in 'aggregates'
-   f = HTSeq.GenomicFeature( aggregate_id, "exonic_part", iv )
-   f.source = os.path.basename( sys.argv[0] )
-   #   f.source = "camara"
-   f.attr = {}
-   f.attr[ 'gene_id' ] = aggregate_id
-   transcript_set = set( ( transcript_id for gene_id, transcript_id in s ) )
-   f.attr[ 'transcripts' ] = '+'.join( transcript_set )
-   aggregates[ aggregate_id ].append( f )
+    if len(s) == 0:
+        continue
+    gene_id = list(s)[0][0]
+    ## if aggregateGenes=FALSE, ignore the exons associated to more than one gene ID
+    if aggregateGenes == False:
+        check_set = set()
+        for geneID, transcript_id in s:
+            check_set.add( geneID )
+            if( len( check_set ) > 1 ):
+                continue
+            else:
+                aggregate_id = gene_id
+    # Take one of the gene IDs, find the others via gene sets, and
+    # form the aggregate ID from all of them
+    else:
+        assert set( gene_id for gene_id, transcript_id in s ) <= gene_sets[ gene_id ]
+        aggregate_id = '+'.join( gene_sets[ gene_id ] )
+        # Make the feature and store it in 'aggregates'
+    f = HTSeq.GenomicFeature( aggregate_id, "exonic_part", iv )
+    f.source = os.path.basename( sys.argv[0] )
+    #   f.source = "camara"
+    f.attr = {}
+    f.attr[ 'gene_id' ] = aggregate_id
+    transcript_set = set( ( transcript_id for gene_id, transcript_id in s ) )
+    f.attr[ 'transcripts' ] = '+'.join( transcript_set )
+    aggregates[ aggregate_id ].append( f )
 
 
 # Step 4: For each aggregate, number the exonic parts
 
 aggregate_features = []
 for l in aggregates.values():
-   for i in range( len(l)-1 ):
-       assert l[i].name == l[i+1].name, str(l[i+1]) + " has wrong name"
-       assert l[i].iv.end <= l[i+1].iv.start, str(l[i+1]) + " starts too early"
-      if l[i].iv.chrom != l[i+1].iv.chrom:
-         raise ValueError(
-             "Same name found on two chromosomes: %s, %s" % ( str(l[i]), str(l[i+1]) )
-         )
-      if l[i].iv.strand != l[i+1].iv.strand:
-         raise ValueError(
-             "Same name found on two strands: %s, %s" % ( str(l[i]), str(l[i+1]) )
-         )
-     aggr_feat = HTSeq.GenomicFeature( l[0].name, "aggregate_gene",
-                                       HTSeq.GenomicInterval( l[0].iv.chrom, l[0].iv.start,
-                                                              l[-1].iv.end, l[0].iv.strand ) )
-     aggr_feat.source = os.path.basename( sys.argv[0] )
-     aggr_feat.attr = { 'gene_id': aggr_feat.name }
-   for i in range( len(l) ):
-       l[i].attr['exonic_part_number'] = "%03d" % ( i+1 )
-       aggregate_features.append( aggr_feat )
+    for i in range( len(l)-1 ):
+        assert l[i].name == l[i+1].name, str(l[i+1]) + " has wrong name"
+        assert l[i].iv.end <= l[i+1].iv.start, str(l[i+1]) + " starts too early"
+        if l[i].iv.chrom != l[i+1].iv.chrom:
+            raise ValueError(
+                "Same name found on two chromosomes: %s, %s" % ( str(l[i]), str(l[i+1]) )
+            )
+        if l[i].iv.strand != l[i+1].iv.strand:
+            raise ValueError(
+                "Same name found on two strands: %s, %s" % ( str(l[i]), str(l[i+1]) )
+            )
+        aggr_feat = HTSeq.GenomicFeature( l[0].name, "aggregate_gene",
+                                          HTSeq.GenomicInterval( l[0].iv.chrom, l[0].iv.start,
+                                                                 l[-1].iv.end, l[0].iv.strand ) )
+        aggr_feat.source = os.path.basename( sys.argv[0] )
+        aggr_feat.attr = { 'gene_id': aggr_feat.name }
+    for i in range( len(l) ):
+        l[i].attr['exonic_part_number'] = "%03d" % ( i+1 )
+        aggregate_features.append( aggr_feat )
 
 
 # Step 5: Sort the aggregates, then write everything out
@@ -176,8 +176,8 @@ else:
 fcountgtf = opts.fcgtf
 
 if fcountgtf :
-	os.system('sed s/aggregate_gene/gene/g ' + out_file + ' > ' + fcountgtf)
-	os.system('sed -i s/exonic_part/exon/g ' + fcountgtf)
-	print("Done!")
+    os.system('sed s/aggregate_gene/gene/g ' + out_file + ' > ' + fcountgtf)
+    os.system('sed -i s/exonic_part/exon/g ' + fcountgtf)
+    print("Done!")
 else :
-	print("Done!")
+    print("Done!")
